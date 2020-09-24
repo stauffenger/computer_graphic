0d6114b128b2740df55289c1488f48067a27f6b8
Luke Stauffenger <lucas.miranda96@hotmail.com>
2020-09-23 21:14:07 -0300

    Drawing a triangle to test the to color function.

--- main.cpp
+++ main.cpp
@@ -11,17 +11,48 @@ void setBackgroundRGBA(Color color) {
     glClear(GL_COLOR_BUFFER_BIT);
 }
 
+void setColorRGBA(Color color) {
+    glColor4f(
+        color.red,
+        color.green,
+        color.blue,
+        color.alpha
+    );
+}
+
+void setBackgroundColor(ColorName targetColor) {
+    Color backgroundColor(targetColor);
+    setBackgroundRGBA(backgroundColor);
+}
+
+void setColor(ColorName targetColor) {
+    Color drawColor(targetColor);
+    setColorRGBA(drawColor);
+}
+
+void drawTriangle() {
+    glBegin(GL_TRIANGLES);
+        glVertex3f(-0.9, -0.9, 0.0);
+        //glVertex3f(0.9, 0.9, 0.0);
+        glVertex3f(0.9, 0.9, 0.0);
+        //glVertex3f(0.9, -0.9, 0.0);
+        glVertex3f(0.9, -0.9, 0.0);
+        //glVertex3f(-0.9, -0.9, 0.0);
+    glEnd();
+}
+
+void draw() {
+    setBackgroundColor(White);
+    setColor(Black);
+    drawTriangle();
+
+    glFlush();
+}
+
 void createWindow() {
     GLint width = 800, height = 600;
     glutInitWindowSize(width, height);
     glutCreateWindow("Chemex Coffeemaker");
-}
-
-void draw() {
-    Color backgroundColor(White);
-    setBackgroundRGBA(backgroundColor);
-
-    glFlush();
 }
 
 int main(int argc, char** argv) {
