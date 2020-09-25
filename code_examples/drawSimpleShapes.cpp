6706eb6ecc71fb996692565a271df432012480c5
Luke Stauffenger <lucas.miranda96@hotmail.com>
2020-09-25 19:10:36 -0300

    Declaring constants.

--- code_examples/drawSimpleShapes.cpp
+++ code_examples/drawSimpleShapes.cpp
@@ -1,29 +1,32 @@
 #include <GL/glut.h>
 #include "drawSimpleShapes.h"
+#define MAXIMUM 0.9
+#define MIDDLE 0.0
+#define MINIMUM -0.9
 
 void drawTriangle() {
     glBegin(GL_TRIANGLES);
-        glVertex3f(-0.9, -0.9, 0.0);
-        glVertex3f(0.9, 0.9, 0.0);
-        glVertex3f(0.9, -0.9, 0.0);
+        glVertex3f(MINIMUM, MINIMUM, MIDDLE);
+        glVertex3f(MAXIMUM, MAXIMUM, MIDDLE);
+        glVertex3f(MAXIMUM, MINIMUM, MIDDLE);
     glEnd();
 }
 
 void drawSquare() {
     glBegin(GL_QUADS);
-        glVertex3f(-0.9, -0.9, 0.0);
-        glVertex3f(-0.9, 0.9, 0.0);
-        glVertex3f(0.9, 0.9, 0.0);
-        glVertex3f(0.9, -0.9, 0.0);
+        glVertex3f(MINIMUM, MINIMUM, MIDDLE);
+        glVertex3f(MINIMUM, MAXIMUM, MIDDLE);
+        glVertex3f(MAXIMUM, MAXIMUM, MIDDLE);
+        glVertex3f(MAXIMUM, MINIMUM, MIDDLE);
     glEnd();
 }
 
 void drawRhombus() {
     glBegin(GL_QUADS);
-        glVertex3f(-0.9, 0.0, 0.0);
-        glVertex3f(0.0, 0.9, 0.0);
-        glVertex3f(0.9, 0.0, 0.0);
-        glVertex3f(0.0, -0.9, 0.0);
+        glVertex3f(MINIMUM, MIDDLE, MIDDLE);
+        glVertex3f(MIDDLE, MAXIMUM, MIDDLE);
+        glVertex3f(MAXIMUM, MIDDLE, MIDDLE);
+        glVertex3f(MIDDLE, MINIMUM, MIDDLE);
     glEnd();
 }
 
